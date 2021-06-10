deca798ce37b19530046aab33a1493eaecdb64bf
Michael Ghen <mike@mikeghen.com>
2021-06-10 12:28:03 -0400

    Switch to tellor oracle

--- 01-Contracts/contracts/ISimpleOracle.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.7.1;
-
-
-interface ISimpleOracle {
-    function update() external;
-    function consult(address token, uint amountIn) external view returns (uint amountOut);
-}
