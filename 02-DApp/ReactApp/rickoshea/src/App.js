0e3e4fed6da2afd24474fd2efcdba470b1a6c0d8
Michael Ghen <mike@mikeghen.com>
2021-06-28 14:10:08 -0400

    update dapp for rinkeby

--- 02-DApp/ReactApp/rickoshea/src/App.js
+++ 02-DApp/ReactApp/rickoshea/src/App.js
@@ -31,11 +31,11 @@ class App extends Component {
       sfUser:null,                 // Superfluid User object (tied to a specific token)
       host:null,                   // Superfluid host contract instance
       ida:null,                    // Superfluid Instant Distribution Agreement contract instance
-      flowAmt:""                   // How much the user has streaming (storing as instance variable so it can be shown on front end)
+      flowAmt:"",                   // How much the user has streaming (storing as instance variable so it can be shown on front end)
+      isSubscribed:false            // True if the user has approved the streaming
     };
 
     this.startFlow = this.startFlow.bind(this);
-    this.approve = this.approve.bind(this);
     this.getOnlySuperAppFlows = this.getOnlySuperAppFlows.bind(this);
   }
 
@@ -83,7 +83,7 @@ class App extends Component {
       // this.setState({
       //   flowAmt: this.getOnlySuperAppFlows()
       // })
-      console.log(this.stat.sfUser)
+      console.log(this.state.sfUser)
       this.getOnlySuperAppFlows()
       console.log( "Net flow", this.state.flowAmt )
 
@@ -103,8 +103,19 @@ class App extends Component {
       })
       console.log("IDA Address:",this.state.ida._address)
 
-    } catch {
-      console.log("ERROR IN WEB3 SET UP")
+      let user = this.state.account;
+      let isSubscribed =  await this.state.ida.methods.getSubscription(
+                                    ETHxAddress,
+                                    rickosheaAppAddress, // publisher
+                                    0, // indexId
+                                    user).call()
+      this.setState({
+        isSubscribed: isSubscribed.approved
+      })
+      console.log("Is Approved?", this.state.isSubscribed)
+
+    } catch(err) {
+      console.log("ERROR IN WEB3 SET UP:", err)
     }
 
     // Handling account switch
@@ -128,26 +139,62 @@ class App extends Component {
     console.log("Address in getTokenBalance: ",userAddress)
     tokenInst.methods.balanceOf(userAddress).call().then(function (bal) {
         // console.log(tokenAddress,'balance is',bal)
-        document.getElementById(`balance-${tokenAddress}`).innerHTML = (bal/1000000000000000000).toFixed(10);
+        document.getElementById(`balance-${tokenAddress}`).innerHTML = (bal/1000000000000000000).toFixed(4);
     })
   }
 
   // Starting a Superfluid flow based on what user selects in field
   async startFlow() {
+    const web3 = new Web3(window.ethereum)
     let sf = this.state.sf
     let sfUser = this.state.sfUser
     console.log("Creating flow with:",sfUser)
     const userData = { message: "here's a flow account 2", flowId: "1" } // This is just arbitrary
-
     let flowInput = Math.round( ( document.getElementById("input-amt-"+ETHxAddress).value * Math.pow(10,18) ) / 2592000 ) // Say I start a stream of 10 USDCx per month. Is the flow in gwei (which is registered as to the second) calculated as [ (10 USDCx) *(10^18) ] / [30 * 24 * 60 * 60]  = 3858024691358.025 -> round to nearest int
     console.log("Would flow:",flowInput)
-    await sfUser.flow({
-      recipient: await sf.user({ address: rickosheaAppAddress, token: fUSDCxAddress }), // address: would be rickosheaAppaddress, currently not deployed
-      flowRate: flowInput.toString(),
-      options: {
-        userData
-      }
-    })
+
+    let isStreaming = false;
+
+    let call = [
+            [
+                201, // approve the ticket fee
+                sf.agreements.ida.address,
+                web3.eth.abi.encodeParameters(
+                  ["bytes", "bytes"],
+                  [
+                      sf.agreements.ida.contract.methods
+                          .approveSubscription(
+                              ETHxAddress,
+                              rickosheaAppAddress,
+                              0, // INDEX_ID
+                              "0x"
+                          )
+                          .encodeABI(), // callData
+                      "0x" // userData
+                  ]
+                )
+            ],
+            [
+              201, // create constant flow (10/mo)
+              sf.agreements.cfa.address,
+              web3.eth.abi.encodeParameters(
+                  ["bytes", "bytes"],
+                  [
+                      sf.agreements.cfa.contract.methods
+                          .createFlow(
+                              fUSDCxAddress,
+                              rickosheaAppAddress,
+                              flowInput.toString(),
+                              "0x"
+                          )
+                          .encodeABI(), // callData
+                      "0x" // userData
+                  ]
+              )
+            ],
+          ]
+
+    await sf.host.batchCall(call);
 
     document.getElementById("input-amt-"+ETHxAddress).value = ""
 
@@ -176,55 +223,7 @@ class App extends Component {
     }
   }
 
-  // Approving SuperApp to send us ETHx
-  // NOT USABLE YET - rickoshea app hasn't been deployed, address is fake
-  async approve() {
-    let indexId = "0";
-    let subscriber = this.state.sfUser.address;
-    // await this.state.sf.agreements.ida.approveSubscription({
-    //     superToken: ETHxAddress,
-    //     indexId: indexId,
-    //     publisher: rickosheaAppAddress,
-    //     subscriber: subscriber,
-    //     userData: "0x"});
-    // //     TODO: refreshSupscription
 
-    await web3tx(
-        this.state.sf.host.callAgreement,
-        "Bob approves subscription to the app"
-    )(
-        this.state.sf.agreements.ida.address,
-        this.state.sf.agreements.ida.contract.methods
-            .approveSubscription(ETHxAddress, rickosheaAppAddress, 0, "0x")
-            .encodeABI(),
-        "0x", // user data
-        {
-            from: subscriber
-        }
-    );
-  }
-
-  // Refreshing to see if user has already approved ETHx subscription
-  // TODO: make it such that you pass in a parameter with the DCA Recieving token so it can vary (not just ETHx)
-  // NOT USABLE YET - rickoshea app hasn't been deployed, address is fake
-  async refreshSubscription() {
-    const sub = await this.state.ida.methods.getSubscription(
-      ETHxAddress,
-      rickosheaAppAddress,
-      0,
-      this.state.sfUser.address
-    ).call()
-    // console.log(sub)
-    if (sub.approved) {
-      console.log("User has already approved SuperApp Subscription")
-      let abtn = document.getElementById("approve-"+ETHxAddress)
-      let sfld = document.getElementById("input-amt-"+ETHxAddress)
-      let fbtn = document.getElementById("startFlowButton")
-      abtn.disabled = true
-      sfld.disabled = false
-      fbtn.disabled = false
-    }
-  }
 
   render() {
     // var flowAmt = parseInt( this.state.userFlowDetails.cfa.netFlow )
@@ -241,34 +240,19 @@ class App extends Component {
             <div class= "col-6">
               <p><span id="wallet-address" class="badge bg-secondary">{this.state.account}</span></p>
             </div>
-            <div class= "col-1">
+            <div class= "col-4">
               <p></p>
-            </div>
-            <div class="col-5">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">USDCx to ETHx</h5>
-                <p>Inbound Rate: <span id='balance-0x0F1D7C55A2B133E000eA10EeC03c774e0d6796e8'>0</span> USDCx</p>
-                <p>Balance: <span id="balance-0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90">0</span> ETHx</p>
-                <div>
-                <input type="text" id="input-amt-0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90" placeholder={"Current Flow: "+ ( -( this.state.flowAmt*(30*24*60*60) )/Math.pow(10,18) ).toFixed(4)  }/>
-                <button id="startFlowButton" onClick={this.startFlow}>Start Flow</button>
-                <button id="approveDistButton" onClick={this.approve}>Approve</button>
-                </div>
-                <p> USDCx/month</p>
-                </div>
-              </div>
             </div>
             <div class="col-4">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">USDCx to UWLx</h5>
-                <p>Inbound Rate: <span id='balance-0x0F1D7C55A2B133E000eA10EeC03c774e0d6796e8'>0</span> USDCx</p>
-                <p>Balance: <span id="balance-0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90">0</span> UWLx</p>
+                <h5 class="card-title">USDCx to ETHx</h5>
+                <p>Your Balance:</p>
+                <p><span id='balance-0x0F1D7C55A2B133E000eA10EeC03c774e0d6796e8'>0</span> USDCx</p>
+                <p><span id="balance-0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90">0</span> ETHx</p>
                 <div>
-                <input type="text" id="input-amt-0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90" placeholder={"Current Flow: "+ ( -( this.state.flowAmt*(30*24*60*60) )/Math.pow(10,18) ).toFixed(4)  }/>
+                <input type="text" id="input-amt-0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90" placeholder={( -( this.state.flowAmt*(30*24*60*60) )/Math.pow(10,18) ).toFixed(4)  }/>
                 <button id="startFlowButton" onClick={this.startFlow}>Start Flow</button>
-                <button id="approveDistButton" onClick={this.approve}>Approve</button>
                 </div>
                 <p> USDCx/month</p>
                 </div>
--- 02-DApp/ReactApp/rickoshea/src/rinkeby_config.js
+++ 02-DApp/ReactApp/rickoshea/src/rinkeby_config.js
@@ -3,4 +3,4 @@ export const fUSDCAddress = "0xbe49ac1EadAc65dccf204D4Df81d650B50122aB2";
 export const ETHxAddress = "0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90"; // DAIx for testing
 export const hostAddress = "0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6";
 export const idaAddress = "0x32E0ecb72C1dDD92B007405F8102c1556624264D";
-export const rickosheaAppAddress = "0x3B3775eB7D4EFb5122Bde89B52E9A1a3813bB4F9";
+export const rickosheaAppAddress = "0x5945Be81b25115B5a68D7587C0B418Bd2c6c4eBE";
