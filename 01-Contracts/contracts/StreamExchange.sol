5d022501f55fbf2070d19dd1d92b82ac270a2fa4
Michael Ghen <mike@mikeghen.com>
2021-08-05 15:32:02 -0400

    work in progress on v2

--- 01-Contracts/contracts/StreamExchange.sol
+++ 01-Contracts/contracts/StreamExchange.sol
@@ -40,20 +40,20 @@ import "./tellor/ITellor.sol";
 
 contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
 
-    // TODO: uint256 public constant RATE_PERCISION = 1000000;
     using SafeERC20 for ERC20;
     using StreamExchangeHelper for StreamExchangeStorage.StreamExchange;
     using StreamExchangeStorage for StreamExchangeStorage.StreamExchange;
     StreamExchangeStorage.StreamExchange internal _exchange;
 
-    event UpdatedStream(address from, int96 newRate, int96 totalInflow);
+    // TODO: This event needs to include which token the streams updated for
+    event UpdatedStream(address from, address token, int96 newRate, int96 totalInflow);
 
     constructor(
         ISuperfluid host,
         IConstantFlowAgreementV1 cfa,
         IInstantDistributionAgreementV1  ida,
-        ISuperToken inputToken,
-        ISuperToken outputToken,
+        ISuperToken tokenA,
+        ISuperToken tokenB,
         ISuperToken subsidyToken,
         IUniswapV2Router02 sushiRouter,
         address payable oracle,
@@ -63,24 +63,34 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
         require(address(host) != address(0), "host");
         require(address(cfa) != address(0), "cfa");
         require(address(ida) != address(0), "ida");
-        require(address(inputToken) != address(0), "inputToken");
-        require(address(outputToken) != address(0), "output");
+        require(address(tokenA) != address(0), "tokenA");
+        require(address(tokenB) != address(0), "tokenB");
         require(!host.isApp(ISuperApp(msg.sender)), "owner SA");
 
         _exchange.sushiRouter = sushiRouter;
         _exchange.host = host;
         _exchange.cfa = cfa;
         _exchange.ida = ida;
-        _exchange.inputToken = inputToken;
-        _exchange.outputToken = outputToken;
+        _exchange.poolA.token = tokenA;
+        _exchange.poolB.token = tokenB;
         _exchange.subsidyToken = subsidyToken;
+        _exchange.poolA.idaIndex = 0;
+        _exchange.poolB.idaIndex = 1;
+        _exchange.subsidyIdaIndex = 2;
         _exchange.oracle = ITellor(oracle);
         _exchange.requestId = requestId;
         _exchange.feeRate = 20000;
         _exchange.rateTolerance = 10000;
-        _exchange.subsidyIndexId = 1;
         _exchange.subsidyRate = 4e17; // 0.4 tokens/second ~ 1,000,000 tokens in a month
         _exchange.owner = msg.sender;
+
+        // Unlimited approve for sushiswap
+        ERC20(_exchange.poolA.token.getUnderlyingToken()).safeIncreaseAllowance(address(_exchange.sushiRouter), 2**256 - 1);
+        ERC20(_exchange.poolB.token.getUnderlyingToken()).safeIncreaseAllowance(address(_exchange.sushiRouter), 2**256 - 1);
+        // and Supertoken upgrades
+        ERC20(_exchange.poolA.token.getUnderlyingToken()).safeIncreaseAllowance(address(_exchange.poolA.token), 2**256 - 1);
+        ERC20(_exchange.poolB.token.getUnderlyingToken()).safeIncreaseAllowance(address(_exchange.poolB.token), 2**256 - 1);
+
 
         uint256 configWord =
             SuperAppDefinitions.APP_LEVEL_FINAL |
@@ -94,11 +104,9 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
             _exchange.host.registerApp(configWord);
         }
 
-        // Set up the IDA for sending tokens back
-        _exchange._createIndex(_exchange.outputIndexId, _exchange.outputToken);
-
-        // Give the owner 1 share just to start up the contract
-        _exchange._updateSubscription(_exchange.outputIndexId, msg.sender, 1, _exchange.outputToken);
+        // Setup tokenA and tokenB IDA Indexes
+        _exchange._createIndex(_exchange.poolA.idaIndex, _exchange.poolA.token);
+        _exchange._createIndex(_exchange.poolB.idaIndex, _exchange.poolB.token);
 
         // Setup Liquidity Mining
         _exchange._initalizeLiquidityMining();
@@ -111,33 +119,46 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
      *************************************************************************/
 
     /// @dev If a new stream is opened, or an existing one is opened
-  function _updateOutflow(bytes calldata ctx, bytes calldata agreementData, bool doDistributeFirst)
+  function _updateOutflow(bytes calldata ctx, bytes calldata agreementData, ISuperToken inputToken, bool doDistributeFirst)
       private
       returns (bytes memory newCtx)
   {
 
+    // TODO: Needs to know which of the tokens this stream is for, either tokenA or tokenB
+    // NOTE: Generally this looks OK, just every instance of input/ouput token needs
+    //       to be replaced and a check to see which token we have is needed
+
     newCtx = ctx;
+    (address requester, address flowReceiver) = abi.decode(agreementData, (address, address));
+
+    StreamExchangeStorage.TokenPool storage pool;
+    if(inputToken == _exchange.poolA.token) {
+      pool = _exchange.poolA;
+    } else {
+      pool = _exchange.poolB;
+    }
 
     (, , uint128 totalUnitsApproved, uint128 totalUnitsPending) = _exchange.ida.getIndex(
-                                                                         _exchange.outputToken,
+                                                                         // TODO: tokenA/B
+                                                                         pool.token,
                                                                          address(this),
-                                                                         _exchange.outputIndexId);
+                                                                         pool.idaIndex);
 
-    if (doDistributeFirst && totalUnitsApproved + totalUnitsPending > 0 && ISuperToken(_exchange.inputToken).balanceOf(address(this)) > 0) {
+    if (doDistributeFirst && totalUnitsApproved + totalUnitsPending > 0 && pool.token.balanceOf(address(this)) > 0) {
       newCtx = _exchange._distribute(newCtx);
     }
 
-    (address requester, address flowReceiver) = abi.decode(agreementData, (address, address));
-    int96 changeInFlowRate = _exchange.cfa.getNetFlow(_exchange.inputToken, address(this)) - _exchange.totalInflow;
+    int96 changeInFlowRate = _exchange.cfa.getNetFlow(pool.token, address(this)) - pool.totalInflow;
+    pool.streams[requester].rate = pool.streams[requester].rate + changeInFlowRate;
 
-    _exchange.streams[requester].rate = _exchange.streams[requester].rate + changeInFlowRate;
+    newCtx = _exchange._updateSubscriptionWithContext(newCtx, pool.idaIndex, requester, uint128(uint(int(pool.streams[requester].rate))), pool.token);
 
-    newCtx = _exchange._updateSubscriptionWithContext(newCtx, _exchange.outputIndexId, requester, uint128(uint(int(_exchange.streams[requester].rate))), _exchange.outputToken);
-    newCtx = _exchange._updateSubscriptionWithContext(newCtx, _exchange.subsidyIndexId, requester, uint128(uint(int(_exchange.streams[requester].rate))), _exchange.subsidyToken);
+    //TODO: Subsidy token will need to contain two pools
+    newCtx = _exchange._updateSubscriptionWithContext(newCtx, _exchange.subsidyIdaIndex, requester, uint128(uint(int(pool.streams[requester].rate))), _exchange.subsidyToken);
 
-    _exchange.totalInflow = _exchange.totalInflow + changeInFlowRate;
+    pool.totalInflow = pool.totalInflow + changeInFlowRate;
 
-    emit UpdatedStream(requester, _exchange.streams[requester].rate, _exchange.totalInflow);
+    emit UpdatedStream(requester, address(pool.token), pool.streams[requester].rate, pool.totalInflow);
 
   }
 
@@ -170,32 +191,34 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
    return _exchange.host.isAppJailed(this);
   }
 
-  function getInputToken() external view returns (ISuperToken) {
-   return _exchange.inputToken;
+  function getInputToken(uint256 pool) external view returns (ISuperToken) {
+    if(pool == 0) {
+      return _exchange.poolA.token;
+    } else {
+      return _exchange.poolA.token;
+    }
   }
 
-  function getOuputToken() external view returns (ISuperToken) {
-   return _exchange.outputToken;
+  function getTotalInflow(uint256 pool) external view returns (int96) {
+    if(pool == 0) {
+      return _exchange.poolA.totalInflow;
+    } else {
+      return _exchange.poolA.totalInflow;
+    }
   }
 
-  function getOuputIndexId() external view returns (uint32) {
-   return _exchange.outputIndexId;
-  }
+  // TODO: More getters for the TokenPools properties
 
   function getSubsidyToken() external view returns (ISuperToken) {
    return _exchange.subsidyToken;
   }
 
-  function getSubsidyIndexId() external view returns (uint32) {
-   return _exchange.subsidyIndexId;
+  function getSubsidyIdaIndex() external view returns (uint32) {
+   return _exchange.subsidyIdaIndex;
   }
 
   function getSubsidyRate() external view returns (uint256) {
     return _exchange.subsidyRate;
-  }
-
-  function getTotalInflow() external view returns (int96) {
-    return _exchange.totalInflow;
   }
 
   function getLastDistributionAt() external view returns (uint256) {
@@ -226,11 +249,15 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
     return _exchange.rateTolerance;
   }
 
-  function getStreamRate(address streamer) external view returns (int96) {
-    return _exchange.streams[streamer].rate;
+  function getStreamRate(address streamer, uint256 pool) external view returns (int96) {
+    if(pool == 0) {
+      return _exchange.poolA.streams[streamer].rate;
+    } else {
+      return _exchange.poolA.streams[streamer].rate;
+    }
   }
 
-  function emergencyCloseStream(address streamer) public {
+  function emergencyCloseStream(address streamer, address token) public {
     // Allows anyone to close any stream iff the app is jailed
     bool isJailed = ISuperfluid(msg.sender).isAppJailed(ISuperApp(address(this)));
     require(isJailed, "!jailed");
@@ -238,7 +265,7 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
         _exchange.cfa,
         abi.encodeWithSelector(
             _exchange.cfa.deleteFlow.selector,
-            _exchange.inputToken,
+            token,
             streamer,
             address(this),
             new bytes(0) // placeholder
@@ -270,12 +297,12 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
       bytes calldata _ctx
   )
       external override
-      onlyExpected(_superToken, _agreementClass)
       onlyHost
       returns (bytes memory newCtx)
   {
-      if (!_exchange._isInputToken(_superToken) || !_exchange._isCFAv1(_agreementClass)) return _ctx;
-      return _updateOutflow(_ctx, _agreementData, true);
+      // TODO: Checks to handle both tokenA and tokenB, this is where it can detected which Supertoken we have
+      if (!_exchange._isAllowedToken(_superToken) || !_exchange._isCFAv1(_agreementClass)) return _ctx;
+      return _updateOutflow(_ctx, _agreementData, _superToken, true);
   }
 
   function afterAgreementUpdated(
@@ -287,12 +314,12 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
       bytes calldata _ctx
   )
       external override
-      onlyExpected(_superToken, _agreementClass)
       onlyHost
       returns (bytes memory newCtx)
   {
-      if (!_exchange._isInputToken(_superToken) || !_exchange._isCFAv1(_agreementClass)) return _ctx;
-      return _updateOutflow(_ctx, _agreementData, true);
+      // TODO: Checks to handle both tokenA and tokenB, this is where it can detected which Supertoken we have
+      if (!_exchange._isAllowedToken(_superToken) || !_exchange._isCFAv1(_agreementClass)) return _ctx;
+      return _updateOutflow(_ctx, _agreementData, _superToken, true);
   }
 
   function afterAgreementTerminated(
@@ -307,10 +334,11 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
       onlyHost
       returns (bytes memory newCtx)
   {
+      // TODO: Checks to handle both tokenA and tokenB, this is where it can detected which Supertoken we have
       // According to the app basic law, we should never revert in a termination callback
-      if (!_exchange._isInputToken(_superToken) || !_exchange._isCFAv1(_agreementClass)) return _ctx;
+      if (!_exchange._isAllowedToken(_superToken) || !_exchange._isCFAv1(_agreementClass)) return _ctx;
       // Skip distribution when terminating to avoid reverts
-      return _updateOutflow(_ctx, _agreementData, false);
+      return _updateOutflow(_ctx, _agreementData, _superToken, false);
   }
 
 
@@ -320,14 +348,5 @@ contract StreamExchange is Ownable, SuperAppBase, UsingTellor {
       _;
   }
 
-  modifier onlyExpected(ISuperToken superToken, address agreementClass) {
-    if (_exchange._isCFAv1(agreementClass)) {
-      require(_exchange._isInputToken(superToken), "!inputAccepted");
-    } else if (_exchange._isIDAv1(agreementClass)) {
-      require(_exchange._isOutputToken(superToken) || _exchange._isSubsidyToken(superToken), "!outputAccepted");
-    }
-    _;
-  }
 
-
-  }
+}
