6b0fd81bb54f2f79e61ccb57d051e469cd7f3c14
rashtrakoff <chinmay.vemuri@outlook.com>
2021-11-04 00:56:34 +0530

    Typo corrected and added optional param.

--- 01-Contracts/tasks/DeployRinkeby.js
+++ 01-Contracts/tasks/DeployRinkeby.js
@@ -4,9 +4,11 @@ task("DeployRinkeby", "Deploys stream exchange contracts on Rinkeby testnet")
     .addParam("router", "Router address (UniswapV2 type router) for swapping")
     .addParam("oracle", "Oracle address for the tokens")
     .addParam("requestid", "Request ID for Tellor oracle")
+    .addOptionalParam("check", "Checks and skips if the StreamExchange contract has already been deployed", true, types.boolean)
     .setAction(async (taskArgs, hre) => {
         const { deploy } = deployments;
         const [deployer] = await ethers.getSigners();
+
 
         // Rinkeby testnet addresses
         const HOST_ADDRESS = "0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6";
@@ -14,7 +16,8 @@ task("DeployRinkeby", "Deploys stream exchange contracts on Rinkeby testnet")
         const IDA_ADDRESS = "0x32E0ecb72C1dDD92B007405F8102c1556624264D";
         const RIC_CONTRACT_ADDRESS = "0x369A77c1A8A38488cc28C2FaF81D2378B9321D8B";
 
-
+        // The following library doesn't use 'check' value as it will most likely remain unchanged
+        // and hence no need for re-deployment.
         const streamExchangeHelper = await deploy("StreamExchangeHelper", {
             from: deployer.address,
             skipIfAlreadyDeployed: true // Make this false in case you are sure that the contract doesn't exist
@@ -37,7 +40,7 @@ task("DeployRinkeby", "Deploys stream exchange contracts on Rinkeby testnet")
             libraries: {
                 StreamExchangeHelper: streamExchangeHelper.address
             },
-            skipIfAlreadyDeployed: true // Make this false in case you are sure that the contract doesn't exist
+            skipIfAlreadyDeployed: taskArgs.check // Make this false in case you are sure that the contract doesn't exist
         });
 
         console.log("Deployed StreamExchangeHelper at: ", streamExchangeHelper.address);
